d1c07a37761352ea713aa25a4cf01d56cf8c546a
elgs <elgs1980@hotmail.com>
2014-06-07 20:38:10 +0800

    Bug fixed.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
 (function () {
     var zonefile = require('../lib/zonefile.js');
-    var options = require('./zonefile_data.json');
+    var options = require('./zonefile_data_forward.json');
     var output = zonefile.generate(options);
+    console.log(output);
+
+    options = require('./zonefile_data_reverse.json');
+    output = zonefile.generate(options);
     console.log(output);
 })();

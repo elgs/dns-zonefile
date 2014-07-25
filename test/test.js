2429a0ba1e6123af74f5fa7e7a442ca98f0a94de
Christoph Witzko <github@christophwitzko.com>
2014-07-25 14:47:25 +0200

    added IPv6 support (AAAA records, RFC3596), some small improvements

--- test/test.js
+++ test/test.js
@@ -11,6 +11,11 @@
     output = zonefile.generate(options);
     console.log(output);
 
+    console.log('##########', 'Generating reverse zone file (IPv6) from JSON', '##########');
+    options = require('./zonefile_reverse_ipv6.json');
+    output = zonefile.generate(options);
+    console.log(output);
+
     console.log('##########', 'Parsing forward zone file to JSON', '##########');
     var text = fs.readFileSync('./zonefile_forward.txt', 'utf8');
     output = zonefile.parse(text);
@@ -19,6 +24,12 @@
     console.log('\n');
     console.log('##########', 'Parsing reverse zone file to JSON', '##########');
     text = fs.readFileSync('./zonefile_reverse.txt', 'utf8');
+    output = zonefile.parse(text);
+    console.log(output);
+
+    console.log('\n');
+    console.log('##########', 'Parsing reverse zone file (IPv6) to JSON', '##########');
+    text = fs.readFileSync('./zonefile_reverse_ipv6.txt', 'utf8');
     output = zonefile.parse(text);
     console.log(output);
 })();

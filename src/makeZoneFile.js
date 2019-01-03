c84d4499e65688db77a89625d6a554d9c459d61b
Aaron Blankstein <aaron@blockstack.com>
2019-01-03 16:46:54 -0600

    pass flow tests in makeZoneFile

--- src/makeZoneFile.js
+++ src/makeZoneFile.js
@@ -4,25 +4,70 @@
 
 import { getZoneFileTemplate } from './zoneFileTemplate'
 
-export function makeZoneFile(jsonZoneFile, template=getZoneFileTemplate()) {
-    template = processOrigin(jsonZoneFile['$origin'], template);
-    template = processTTL(jsonZoneFile['$ttl'], template);
-    template = processSOA(jsonZoneFile['soa'], template);
-    template = processNS(jsonZoneFile['ns'], template);
-    template = processA(jsonZoneFile['a'], template);
-    template = processAAAA(jsonZoneFile['aaaa'], template);
-    template = processCNAME(jsonZoneFile['cname'], template);
-    template = processMX(jsonZoneFile['mx'], template);
-    template = processPTR(jsonZoneFile['ptr'], template);
-    template = processTXT(jsonZoneFile['txt'], template);
-    template = processSRV(jsonZoneFile['srv'], template);
-    template = processSPF(jsonZoneFile['spf'], template);
-    template = processURI(jsonZoneFile['uri'], template);
+type SoaType = { name?: string, ttl?: number|string }
+type NSEntryType = { name?: string, ttl?: number, host: string }
+type AType = { name?: string, ttl?: number, ip: string }
+type CNAMEType = { name?: string, ttl?: number, alias: string }
+type MXType = { name?: string, ttl?: number, host: string, preference: number }
+type TXTType = { name?: string, ttl?: number,
+                 txt: string | [string] }
+type SRVType = { name?: string, ttl?: number,
+                 priority: number, weight: number, port: number, target: string }
+type SPFType = { name?: string, ttl?: number,
+                 data: string }
+type URIType = { name?: string, ttl?: number,
+                 priority: number, weight: number, target: string }
+
+
+export type ZoneFileObject = {
+  '$origin'?: string,
+  '$ttl'?: number,
+  'soa'?: SoaType,
+  'ns'?: [NSEntryType],
+  'a'?: [AType],
+  'aaaa'?: [AType],
+  'cname'?: [CNAMEType],
+  'mx'?: [MXType],
+  'ptr'?: [NSEntryType],
+  'txt'?: [TXTType],
+  'srv'?: [SRVType],
+  'spf'?: [SPFType],
+  'uri'?: [URIType]
+}
+
+export function makeZoneFile(jsonZoneFile: ZoneFileObject,
+                             template: string =getZoneFileTemplate()) {
+    if (jsonZoneFile['$origin'])
+      template = processOrigin(jsonZoneFile['$origin'], template);
+    if (jsonZoneFile['$ttl'])
+      template = processTTL(jsonZoneFile['$ttl'], template);
+    if (jsonZoneFile['soa'])
+      template = processSOA(jsonZoneFile['soa'], template);
+    if (jsonZoneFile['ns'])
+      template = processNS(jsonZoneFile['ns'], template);
+    if (jsonZoneFile['a'])
+      template = processA(jsonZoneFile['a'], template);
+    if (jsonZoneFile['aaaa'])
+      template = processAAAA(jsonZoneFile['aaaa'], template);
+    if (jsonZoneFile['cname'])
+      template = processCNAME(jsonZoneFile['cname'], template);
+    if (jsonZoneFile['mx'])
+      template = processMX(jsonZoneFile['mx'], template);
+    if (jsonZoneFile['ptr'])
+      template = processPTR(jsonZoneFile['ptr'], template);
+    if (jsonZoneFile['txt'])
+      template = processTXT(jsonZoneFile['txt'], template);
+    if (jsonZoneFile['srv'])
+      template = processSRV(jsonZoneFile['srv'], template);
+    if (jsonZoneFile['spf'])
+      template = processSPF(jsonZoneFile['spf'], template);
+    if (jsonZoneFile['uri'])
+      template = processURI(jsonZoneFile['uri'], template);
     template = processValues(jsonZoneFile, template);
     return template.replace(/\n{2,}/gim, '\n\n');
 };
 
-function processOrigin (data, template) {
+function processOrigin(data, template) {
     let ret = '';
     if (typeof data !== 'undefined') {
         ret += '$ORIGIN ' + data;
@@ -30,7 +75,7 @@ function processOrigin (data, template) {
     return template.replace('{$origin}', ret);
 };
 
-let processTTL = function(data, template) {
+function processTTL(data, template) {
     let ret = '';
     if (typeof data !== 'undefined') {
         ret += '$TTL ' + data;
@@ -38,7 +83,7 @@ let processTTL = function(data, template) {
     return template.replace('{$ttl}', ret);
 };
 
-let processSOA = function(data, template) {
+function processSOA(data, template) {
     let ret = template;
     if (typeof data !== 'undefined') {
         data.name = data.name || '@';
@@ -51,74 +96,74 @@ let processSOA = function(data, template) {
     return ret;
 };
 
-let processNS = function(data, template) {
+function processNS(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
-        ret += 'IN\tNS\t' + data[i].host + '\n';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
+        ret += 'IN\tNS\t' + record.host + '\n';
     }
     return template.replace('{ns}', ret);
 };
 
-let processA = function(data, template) {
+function processA(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
-        ret += 'IN\tA\t' + data[i].ip + '\n';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
+        ret += 'IN\tA\t' + record.ip + '\n';
     }
     return template.replace('{a}', ret);
 };
 
-let processAAAA = function(data, template) {
+function processAAAA(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
-        ret += 'IN\tAAAA\t' + data[i].ip + '\n';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
+        ret += 'IN\tAAAA\t' + record.ip + '\n';
     }
     return template.replace('{aaaa}', ret);
 };
 
-let processCNAME = function(data, template) {
+function processCNAME(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
-        ret += 'IN\tCNAME\t' + data[i].alias + '\n';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
+        ret += 'IN\tCNAME\t' + record.alias + '\n';
     }
     return template.replace('{cname}', ret);
 };
 
-let processMX = function(data, template) {
+function processMX(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
-        ret += 'IN\tMX\t' + data[i].preference + '\t' + data[i].host + '\n';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
+        ret += 'IN\tMX\t' + record.preference + '\t' + record.host + '\n';
     }
     return template.replace('{mx}', ret);
 };
 
-let processPTR = function(data, template) {
+function processPTR(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
-        ret += 'IN\tPTR\t' + data[i].host + '\n';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
+        ret += 'IN\tPTR\t' + record.host + '\n';
     }
     return template.replace('{ptr}', ret);
 };
 
-let processTXT = function(data, template) {
+function processTXT(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
         ret += 'IN\tTXT\t';
-        const txtData = data[i].txt
-        if (txtData instanceof String || typeof(txtData) === 'string') {
+        const txtData = record.txt
+        if (typeof(txtData) === 'string') {
           ret += '"' + txtData + '"';
         } else if (txtData instanceof Array) {
           ret += txtData
@@ -133,43 +178,44 @@ let processTXT = function(data, template) {
     return template.replace('{txt}', ret);
 };
 
-let processSRV = function(data, template) {
+function processSRV(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
-        ret += 'IN\tSRV\t' + data[i].priority + '\t';
-        ret += data[i].weight + '\t';
-        ret += data[i].port + '\t';
-        ret += data[i].target + '\n';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
+        ret += 'IN\tSRV\t' + record.priority + '\t';
+        ret += record.weight + '\t';
+        ret += record.port + '\t';
+        ret += record.target + '\n';
     }
     return template.replace('{srv}', ret);
 };
 
-let processSPF = function(data, template) {
+function processSPF(data, template) {
     let ret = '';
-    for (let i in data) {
-        ret += (data[i].name || '@') + '\t';
-        if (data[i].ttl) ret += data[i].ttl + '\t';
-        ret += 'IN\tSPF\t' + data[i].data + '\n';
+    for (const record of data) {
+        ret += (record.name || '@') + '\t';
+        if (record.ttl) ret += record.ttl + '\t';
+        ret += 'IN\tSPF\t' + record.data + '\n';
     }
     return template.replace('{spf}', ret);
 };
 
-let processURI = function(data, template) {
+function processURI(data, template) {
     let ret = '';
-    for (let i in data) {
-       ret += (data[i].name || '@') + '\t';
-       if (data[i].ttl) ret += data[i].ttl + '\t';
-       ret += 'IN\tURI\t' + data[i].priority + '\t';
-       ret += data[i].weight + '\t';
-       ret += '"' + data[i].target + '"\n';
+    for (const record of data) {
+       ret += (record.name || '@') + '\t';
+       if (record.ttl) ret += record.ttl + '\t';
+       ret += 'IN\tURI\t' + record.priority + '\t';
+       ret += record.weight + '\t';
+       ret += '"' + record.target + '"\n';
     }
     return template.replace('{uri}', ret);
 };
 
-let processValues = function(jsonZoneFile, template) {
-    template = template.replace('{zone}', jsonZoneFile['$origin'] || jsonZoneFile['soa']['name'] || '');
+function processValues(jsonZoneFile, template) {
+    template = template.replace('{zone}', jsonZoneFile['$origin'] || (jsonZoneFile['soa'] ? jsonZoneFile['soa']['name']: false) || '');
     template = template.replace('{datetime}', (new Date()).toISOString());
-    return template.replace('{time}', Math.round(Date.now() / 1000));
+    const time = Math.round(Date.now() / 1000)
+    return template.replace('{time}', `${time}`);
 };
